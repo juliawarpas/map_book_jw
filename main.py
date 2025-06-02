81864c4f07a89186945a7df8bf66396431a5c790
juliawarpas <julia.warpas@student.wat.edu.pl>
2025-06-02 11:10:53 +0200

    wip: crud for formularz

--- main.py
+++ main.py
@@ -1,6 +1,77 @@
 from tkinter import *
 
 import tkintermapview
+from click import command
+
+users: list = []
+
+def add_users() -> None:
+    name = entry_imie.get()
+    surname = entry_nazwisko.get()
+    location = entry_miejscowosc.get()
+    post = entry_post.get()
+
+    user = {'name': name, 'surname': surname, 'location': location, 'post': post}
+    users.append(user)
+
+    print(users)
+
+
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_post.delete(0, END)
+
+    entry_imie.focus()
+    show_users()
+
+def show_users() -> None:
+    listbox_lista_obiektow.delete(0, END)
+    for idx,user in enumerate(users):
+        listbox_lista_obiektow.insert(idx, f'{idx+1}. {user["name"]}')
+
+def remove_user():
+    i=listbox_lista_obiektow.index(ACTIVE)
+    print(i)
+    users.pop(i)
+    show_users()
+
+
+def edit_user():
+    i=listbox_lista_obiektow.index(ACTIVE)
+    name = users[i]['name']
+    surname = users[i]['surname']
+    location = users[i]['location']
+    post = users[i]['post']
+
+    entry_imie.insert(0, name)
+    entry_nazwisko.insert(0, surname)
+    entry_miejscowosc.insert(0, post)
+    entry_post.insert(0, post)
+
+    button_dodaj_obiekt.configure(text='Zapisz', command=lambda: update_user(i))
+
+def update_user(i):
+    name = entry_imie.get()
+    surname = entry_nazwisko.get()
+    location = entry_miejscowosc.get()
+    post = entry_post.get()
+
+    users[i]['name'] = name
+    users[i]['surname'] = surname
+    users[i]['location'] = location
+    users[i]['post'] = post
+
+    show_users()
+
+    button_dodaj_obiekt.config(text='Dodaj', command=add_users)
+
+    entry_imie.insert(0, name)
+    entry_nazwisko.insert(0, surname)
+    entry_miejscowosc.insert(0, post)
+    entry_post.insert(0, post)
+
+    entry_imie.focus()
 
 root = Tk()
 root.geometry("1200x700")
@@ -29,11 +100,11 @@ button_pokaz_szczegoly=Button(ramka_lista_obiektow, text='Pokaz szczegóły')
 button_pokaz_szczegoly.grid(row=2, column=0)
 
 
-button_usun_obiekt=Button(ramka_lista_obiektow, text='Usuń')
+button_usun_obiekt=Button(ramka_lista_obiektow, text='Usuń', command=remove_user)
 button_usun_obiekt.grid(row=2, column=1)
 
 
-button_edytuj_obiekt=Button(ramka_lista_obiektow, text='Edytuj')
+button_edytuj_obiekt=Button(ramka_lista_obiektow, text='Edytuj', command=edit_user)
 button_edytuj_obiekt.grid(row=2, column=2)
 
 # ramka_formularz
@@ -66,7 +137,7 @@ entry_post=Entry(ramka_formularz)
 entry_post.grid(row=4, column=1)
 
 
-button_dodaj_obiekt=Button(ramka_formularz, text='Dodaj')
+button_dodaj_obiekt=Button(ramka_formularz, text='Dodaj', command=add_users)
 button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
 
 # ramka_szczegoly_obiektow
@@ -86,8 +157,8 @@ label_szczegoly_obiektu_surname=Label(ramka_szczegoly_obiektu, text='Nazwisko:')
 label_szczegoly_obiektu_surname.grid(row=1, column=2)
 
 
-label_szczegoly_obiektu_surname_wartosci=Label(ramka_szczegoly_obiektu, text='....')
-label_szczegoly_obiektu_name_wartosc.grid(row=1, column=3)
+label_szczegoly_obiektu_surname_wartosc=Label(ramka_szczegoly_obiektu, text='....')
+label_szczegoly_obiektu_surname_wartosc.grid(row=1, column=3)
 
 
 label_szczegoly_obiektu_miejscowosc=Label(ramka_szczegoly_obiektu, text='Miejscowość:')
